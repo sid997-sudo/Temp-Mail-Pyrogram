588a3fe351863839a997dd5411d09600f4c6873b
Ali Hatami <aliht.workspace@gmail.com>
2023-06-23 00:07:58 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# enter your bot token and api hash
+token = "BOT TOKEN"
+api_hash = "API HASH"
+api_id = 123456
+admin_id = 123456
+
+# Mysql Connection settings
+host = "localhost"
+user = "ROOT"
+password = "ROOT"
+database = "DATABSE"
